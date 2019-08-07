6deeefc1a3d17fb3c37760c1cc8d5d5febd2022d
eou <whythuss@gmail.com>
2019-08-07 18:12:25 -0500

    slowly walk in

--- src/interpreter/interpreter.ts
+++ src/interpreter/interpreter.ts
@@ -8,12 +8,12 @@ import { AST } from "./php-parser/src/ast";
 import * as Parser from "./php-parser/src/index.js";
 
 /**
- * @param {object}  ast - abstract syntax tree
- * @param {Evaluator} evl - evaluator which follow language rules to convert parse tree to a value
- *                          and read and modify the environment as needed
- * @param {Record}  ini - options from `php.ini` file, each setting corresponds to a key in map
- * @param {String}  res - final interpreted result
- * @param {String}  src - PHP source code
+ * @param {object}     ast - abstract syntax tree
+ * @param {Evaluator}  evl - evaluator which follow language rules to convert parse tree to a value
+ *                           and read and modify the environment as needed
+ * @param {Record}     ini - options from `php.ini` file, each setting corresponds to a key in map
+ * @param {String}     res - final interpreted result
+ * @param {String}     src - PHP source code
  */
 class Interpreter {
     public ast: AST;
