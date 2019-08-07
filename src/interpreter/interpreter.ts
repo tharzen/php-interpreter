7868ead1d04d87cf6673ca66c496772e22d45a1b
eou <whythuss@gmail.com>
2019-08-06 19:04:28 -0500

    change structure

--- src/interpreter/interpreter.ts
+++ src/interpreter/interpreter.ts
@@ -5,6 +5,7 @@
 
 import { Evaluator } from "./evaluator/evaluator";
 import { AST } from "./php-parser/src/ast";
+import * as Parser from "./php-parser/src/index.js";
 
 /**
  * @param {object}  ast - abstract syntax tree
@@ -12,22 +13,34 @@ import { AST } from "./php-parser/src/ast";
  *                          and read and modify the environment as needed
  * @param {Record}  ini - options from `php.ini` file
  * @param {String}  res - final interpreted result
+ * @param {String}  src - PHP source code
  */
 class Interpreter {
     public ast: AST;
     public evl: Evaluator;
     public ini: Record<string, boolean>;
     public res: string;
+    public src: string;
     public run: () => void;
-    constructor(ast: AST, ini?: any) {
-        this.ast = ast;
-        this.evl = new Evaluator(this.ast);
+    constructor(src: string, ini?: any) {
+        this.src = src;
         this.ini = ini;
     }
 }
 
 Interpreter.prototype.run = function() {
+    // initialize parser
+    const parser = new Parser({
+        ast: {
+            withPositions: true,
+        },
+        parser: {
+            locations: true,
+        },
+    });
     // preset some options such as from `php.ini`: this.ini and then evaluate AST
+    this.ast = parser.parseCode(this.src);
+    this.evl = new Evaluator(this.ast);
     if (this.ast.children.length > 0) {
         this.evl.eval();
     }
--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,6 @@
  * @description This file is the main entry for interpreting PHP.
  */
 import { Interpreter } from "./interpreter/interpreter";
-import engine from "./interpreter/php-parser/src/index.js";
 
-const parser = new engine({
-    ast: {
-        withPositions: true,
-    },
-    parser: {
-        locations: true,
-    },
-});
-
-// Retrieve the AST from the specified source
-// var eval = parser.parseEval('echo "My first PHP script!";');
-const ast = parser.parseCode("<?php $a = 1 ?>");
-const interpreter = new Interpreter(ast);
+const interpreter = new Interpreter("<?php $a = 1 ?>");
 interpreter.run();
